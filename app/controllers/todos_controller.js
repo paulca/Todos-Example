c10bef40bab07fb8bb42582b87f95ed5c7dc2990
Thorben Schröder <stillepost@gmail.com>
2011-07-09 18:42:37 +0200

    Added the last needed piecs to make everything work
    - updated eyeballs
    - added functionality for mark all complete checkbox
    - added working stats (entry counter)
    - added functionality for clear completed button

--- app/controllers/todos_controller.js
+++ app/controllers/todos_controller.js
@@ -1,9 +1,32 @@
 o_O('TodosController', {
+  todos: o_O('TodosBucket'),
+  
   index: function() {
-    var todos = o_O('TodosBucket')
+    var self = this
     o_O.render('todos/layout', {}, {html: 'body'}, function() {
-      o_O.bind_form(todos, Todo, '#new-todo')
-      o_O.render('todos/todo', todos, '#todo-list', '<li/>')
+      o_O.bind_form(self.todos, Todo, '#new-todo')
+      o_O.render('todos/stats', self.todos, '#stats')
+      o_O.render('todos/todo', self.todos, '#todo-list', '<li/>')
+    })
+  },
+  mark_all_done: function() {
+    var todos = o_O('TodosController').todos
+    var checked = $('.mark-all-done').attr('checked')
+    
+    todos.batch_operation(function(bucket) {
+      bucket.each(function(el) {
+        el('done', checked)
+      })
+    })
+    
+    return true;
+  },
+  clear_completed: function() {
+    var todos = o_O('TodosController').todos
+    todos.batch_operation(function(bucket) {
+      bucket.remove(function(el) {
+        return el('done')
+      })
     })
   }
 });
